d9144664914d8dd4a24b1173183364ac41feb082
shrutiranawade <ranawadeshruti@gmail.com>
2023-03-15 19:42:55 +0530

    mycorrection

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -5,7 +5,7 @@ public class Email {
     private String emailId;
     private String password;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -18,48 +18,58 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        int isCapital =0; int isLowerCase =0;  int isdigit =0;int isSpecialChar =0;
-   if(oldPassword == password){
 
-       if(newPassword.length()>=8){
-           //uppercase
+        if (oldPassword == password) {
 
-           for(int i=0;i<newPassword.length();i++){
-              char ch = newPassword.charAt(i);
-               if(ch >=65 && ch<=90){
-                   isCapital++;
-               }
+            if (isValidPassword(newPassword)) {
 
-           //lowercase
-              else if(ch >='a' && ch <='z'){
-                   isLowerCase++;
-               }
-//digit
-           else if(Character.isDigit(ch)){
-                   isdigit++;
-               }
-               else {
-                   isSpecialChar++;
-               }
-           }
-           if(isCapital >0||isLowerCase >0||isdigit>0||isSpecialChar>0){
-               this.password = newPassword;
-               System.out.println("Password changed Successfully.");
-           }
-       }
-       else{
-           System.out.println("Short length password.");
-       }
-   }
-   else{
-       System.out.println("Password doesn't matching.Please enter correct Password.");
-   }
+                System.out.println("Password changed Successfully.");
+            } else {
+                System.out.println("Short length password.");
+            }
+        }
+        else {
+            System.out.println("Password doesn't matching.Please enter correct Password.");
+        }
     }
+
+
+
+    public boolean isValidPassword(String password) {
+        boolean isCapital = false;
+        boolean isLowerCase = false;
+        boolean isdigit = false;
+        boolean isSpecialChar = false;
+
+        if (password.length()< 8) {
+            return false;
+        }
+
+            for (int i = 0; i < password.length(); i++) {
+                char ch = password.charAt(i);
+                if (ch >= 65 && ch <= 90) {
+                    isCapital = true;
+                }
+                else if (ch >= 'a' && ch <= 'z') {
+                    isLowerCase = true;
+                }
+                else if (Character.isDigit(ch)) {
+                    isdigit = true;
+                }
+                else {
+                    isSpecialChar = true;
+                }
+            }
+        if (isCapital && isLowerCase && isdigit && isSpecialChar) {
+            return true;
+            }
+        else return false;
+            }
 }
